8f7a5793a9f1c5848bc77d09aab838124e9ccadb
Samuel Tesla <samuel.tesla@gmail.com>
2020-11-22 21:18:05 -0600

    strip leading/trailing whitespace from text input

--- huse/__init__.py
+++ huse/__init__.py
@@ -12,13 +12,14 @@ from .markup import *
 def index():
     if request.method == 'GET':
         return render_template("huse-form.html")
-    text = request.form['text']
+    text = request.form['text'].strip()
     codes = re.findall(ANSI_CODE, text)
     values = dict()
     for code in codes:
         values[code] = values.get(code, 0) + 1
-    values_sorted = sorted(values.items(), key=lambda a: a[1], reverse=True)
-    values_sorted = [(colorFromCode(code), code, count) for code, count in values_sorted]
-    mucode = ' '.join(code + re.sub(r'[|{]', '', code) for _, code, count in values_sorted)
+    values_sorted = [(colorFromCode(code), code, count)
+            for code, count in sorted(values.items(), key=lambda a: a[1], reverse=True)]
+    mucode = ' '.join(code + re.sub(r'[|{]', '', code)
+            for _, code, count in values_sorted)
     html = ''.join(t.to_html() for t in parse(text))
     return render_template("huse-result.html", values=values_sorted, mucode=mucode, html=html)
